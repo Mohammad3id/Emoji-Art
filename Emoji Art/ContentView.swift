1667c0e94116701bba39bf0bb7ff281916dc49ad
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2025-02-01 11:21:47 +0200

    Initial Commit

---
+++ Emoji Art/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emoji Art
+//
+//  Created by Mohammad Eid on 01/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Mohammad Eid on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
