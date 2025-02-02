454ea61660ba93742b4810e67b30fda4ffae24ed
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2025-02-02 09:30:12 +0200

    add emoji palettes

---
+++ Emoji Art/Palette/PaletteSelector.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteChoser.swift
+//  Emoji Art
+//
+//  Created by Mohammad Eid on 02/02/2025.
+//
+
+import SwiftUI
+
+struct PaletteSelector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteSelector()
+}
